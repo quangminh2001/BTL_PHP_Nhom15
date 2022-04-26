8dcab5121e5e4a3c6dda2027fcb5f12c51f52782
Nguyen Quang Minh <65820407+quangminh2001@users.noreply.github.com>
2022-04-26 23:23:35 +0700

    revise

--- BTL_PHP_Nhom15/config.php
+++ BTL_PHP_Nhom15/config.php
@@ -2,8 +2,6 @@
 $servername = "localhost";
 $username = "root";
 $password = "Minh2001@";
-$u = $_POST["username"];
-$p = $_POST["password"];
 $conn = mysqli_connect($servername,$username,$password,"thegioididong");
 if(!$conn){
     die("Lỗi kết nối: ".mysqli_connect_error());
--- BTL_PHP_Nhom15/login.php
+++ BTL_PHP_Nhom15/login.php
@@ -1,10 +1,14 @@
 <?php
 include 'config.php';
+
+$u = $_POST["username"];
+$p = $_POST["password"];
+
 $sql = "select * from user where username ='$u' and password = '$p'";
 $result = mysqli_query($conn,$sql);
 
 if(mysqli_num_rows($result) > 0){
-    header("Location: adminpage.html");
+    header("Location: ./Admin/adminpage.html");
 }else{
     header("Location: login.html");
 }
